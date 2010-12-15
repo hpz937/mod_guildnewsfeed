b0dac27d86503ca91d7ec520082ae5de50d313e6
hpz937 <hpz937@gmail.com>
2010-12-15 16:31:02 -0600

    Fix encoding issue

--- helper.php
+++ helper.php
@@ -27,7 +27,8 @@ class guildnewsfeed {
     $i=0;
     $data=array();
     while(!feof($fp)) {
-      $data[]=fgets($fp);
+      $data[]=htmlentities(fgets($fp));
+	  $i++;
       if($i>=guildnewsfeed::$itemstodisplay)
         break;
     }
